e6101131f4c758d88a8fd0a9807a1c1af211fd6f
e-cealgarra <44615545+e-cealgarra@users.noreply.github.com>
2018-10-30 11:41:24 -0600

    Update filename.r

--- filename.r
+++ filename.r
@@ -1,35 +1,8 @@
-#utility functions
-
-readinteger <- function()
-{ 
-  n <- readline(prompt="Enter an integer: ")
-  if(!grepl("^[0-9]+$",n))
-  {
-    return(readinteger())
-  }
-  return(as.integer(n))
-}
-
-# real program start here
-  
-num <- round(runif(1) * 100, digits = 0)
-guess <- -1
-
-cat("Guess a number between 0 and 100.\n")
-
-while(guess != num)
-{ 
-  guess <- readinteger()
-  if (guess == num)
-  {
-    cat("Congratulations,", num, "is right.\n")
-  }
-  else if (guess < num)
-  {
-    cat("It's bigger!\n")
-  }
-  else if(guess > num)
-  {
-    cat("It's smaller!\n")
-  }
-}
+a <- 42
+A <- a * 2  # R is case sensitive
+print(a)
+cat(A, "\n") # "84" is concatenated with "\n"
+if(A>a) # true, 84 > 42
+{
+  cat(A, ">", a, "\n")
+} 
